b12a9355ca2ac343fb25a0b12830947fe21eecf4
Ivan Behichev <ivanbehichev@gmail.com>
2025-04-10 01:19:32 +0300

    Initial Commit

---
+++ NetworkApp/NetworkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkAppApp.swift
+//  NetworkApp
+//
+//  Created by Ivan Behichev on 10.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct NetworkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
